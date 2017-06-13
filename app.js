e95907df31a0d40252fa4a79e396c21e993f84ee
sklarqui <sklarqui@purdue.edu>
2017-06-13 09:58:15 -0400

    added a font

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 class App{
-constructor(selectors){
-        this.flicks=[]
-        this.max=0
-        this.currentCat=''
-        this.list=document.querySelector(selectors.listSelector)
-        this.catList=document.querySelector(selectors.catagoryList)
-        this.template=document.querySelector(selectors.templateSelector)
-        this.catTemplate=document.querySelector(selectors.catagoryTemp)
-        document
-        .querySelector(selectors.catagorySelector)
-        .addEventListener('submit',this.addResponseViaForm.bind(this))
-        this.load()  
-}
+    constructor(selectors){
+            this.flicks=[]
+            this.max=0
+            this.currentCat=''
+            this.list=document.querySelector(selectors.listSelector)
+            this.catList=document.querySelector(selectors.catagoryList)
+            this.template=document.querySelector(selectors.templateSelector)
+            this.catTemplate=document.querySelector(selectors.catagoryTemp)
+            document
+            .querySelector(selectors.catagorySelector)
+            .addEventListener('submit',this.addResponseViaForm.bind(this))
+            this.load()  
+    }
     addResponseViaForm(ev) {
         ev.preventDefault()
         const movieName= ev.target.response.value
@@ -23,49 +23,46 @@ constructor(selectors){
             catagory: f.catagory.value,
             good: 'ehh',
             }
-        this.addFlick(flick
-        )
-        console.log(flick.catagory)
+        this.addFlick(flick)
         this.currentCat=flick.catagory
         this.differentCat()
         f.reset()
     }
     addFlick(flick){
 
-const index =this.flicks.findIndex((currentFlick, i) =>{
-    return currentFlick.catagory===flick.catagory
-})
+        const index =this.flicks.findIndex((currentFlick, i) =>{
+            return currentFlick.catagory===flick.catagory
+        })
 
-if(flick.catagory===this.currentCat){
- const listItem =this.renderListItem(flick)
-   this.list.insertBefore(listItem,this.list.firstChild)
+        if(flick.catagory===this.currentCat){
+            const listItem =this.renderListItem(flick)
+            this.list.insertBefore(listItem,this.list.firstChild)
+            }
+
+        if(index===-1){
+            const cat =this.renderCatagories(flick)
+            this.catList.insertBefore(cat,this.catList.firstChild)
+        }
+        this.max++
+        this.flicks.unshift(flick)
+        this.save()
+
     }
 
-if(index===-1){
-   const cat =this.renderCatagories(flick)
-   this.catList.insertBefore(cat,this.catList.firstChild)
-}
-this.max++
-this.flicks.unshift(flick)
-this.save()
-
-}
-
 renderCatagories(flick){
-const item =this.catTemplate.cloneNode(true)
- item.querySelector('.catagory-name').textContent=flick.catagory
-item.dataset.catagory= flick.catagory
-item.classList.remove('template')
-item.addEventListener('click',this.newCatagory.bind(this))
-return item
+    const item =this.catTemplate.cloneNode(true)
+    item.querySelector('.catagory-name').textContent=flick.catagory
+    item.dataset.catagory= flick.catagory
+    item.classList.remove('template')
+    item.addEventListener('click',this.newCatagory.bind(this))
+    return item
 }
 
 
- renderListItem(flick){
-        const item =this.template.cloneNode(true)//document.createElement('.flick.template')
-       item.querySelector('.flick-name').textContent=flick.name
-       item.dataset.catagory=flick.catagory
-      //  item.querySelector('.flick-year').textContent=flick.cata
+    renderListItem(flick){
+         const item =this.template.cloneNode(true)
+        item.querySelector('.flick-name').textContent=flick.name
+        item.dataset.catagory=flick.catagory
         item.dataset.id = flick.id
         item.classList.remove('template')
         item.querySelector('.remove').addEventListener('click',this.removeMovie.bind(this))
@@ -73,94 +70,65 @@ return item
         item.querySelector('.up').addEventListener('click',this.flipUp.bind(this))
         item.querySelector('.down').addEventListener('click',this.flipDown.bind(this))
         item.querySelector('.flick-name').addEventListener('input',this.edited.bind(this))
-   if(flick.good==='red'){
-       this.prom(item)
-   }
-    return item   
-}
+        if(flick.good==='red'){
+            this.prom(item)
+        }
+            return item   
+    }
 
-removeMovie(ev) {
-const listI=ev.target.closest('.flick')
-console.log(listI)
-listI.remove()
-
-
-//remove from arrray in general splice(?,1)
-
-for(let i =0; i < this.flicks.length;i++){
-    console.log(listI.dataset.id)
-    
-    const currentId=this.flicks[i].id.toString()
-    console.log(currentId)
-if(currentId===listI.dataset.id){
-    this.flicks.splice(i,1)
-    this.save()
-    break
-}
-
-}
-//JSON
-}
-save(){
-    console.log(this.flicks)
-localStorage.setItem('flicks',JSON.stringify(this.flicks))
-}
-
-load(){
-//get the json string out of local storage, 
-const flicksJSON=localStorage.getItem('flicks')
-//turn it into an array
-const flicksArray=JSON.parse(flicksJSON)
-//this.flicks=flicksArray
-console.log(flicksArray)
-if(flicksArray){
-flicksArray.reverse().map(this.addFlick.bind(this))
-
-}
-for(let i=0;i<this.flicks.length;i++){
-if(this.flicks[i].id>this.max){
-this.max=this.flicks[i].id+1
-}
-}
-}
-
-
- flipUp(ev){
-
+    removeMovie(ev) {
         const listI=ev.target.closest('.flick')
+        listI.remove()
+        for(let i =0; i < this.flicks.length;i++){
+            const currentId=this.flicks[i].id.toString()
+        if(currentId===listI.dataset.id){
+            this.flicks.splice(i,1)
+            this.save()
+            break
+        }
+
+        }
+    }
+    save(){
+        localStorage.setItem('flicks',JSON.stringify(this.flicks))
+    }
+
+    load(){
+        const flicksJSON=localStorage.getItem('flicks')
+        const flicksArray=JSON.parse(flicksJSON)
+        if(flicksArray){
+            flicksArray.reverse().map(this.addFlick.bind(this))
+        }
+        for(let i=0;i<this.flicks.length;i++){
+            if(this.flicks[i].id>this.max){
+                this.max=this.flicks[i].id+1
+            }
+        }
+    }
 
 
-
+    flipUp(ev){
+        const listI=ev.target.closest('.flick')
         if(this.list.firstChild!==listI){
             const prevSib=listI.previousSibling
             this.list.insertBefore(listI,prevSib)
-
         }
-        console.log(listI)
-let indexes=[]
-        for(let i =0; i < this.flicks.length;i++){
-            const currentId=this.flicks[i].id.toString()
-           
-
-        if(currentId===listI.dataset.id&&i>0){
-          
-            if(indexes.length>0){
-                console.log(this.flicks[i])
-                console.log(this.flicks[indexes[indexes.length-1]])
-                const temp=this.flicks[i]
-                this.flicks[i]=this.flicks[indexes[indexes.length-1]]
-                this.flicks[indexes[indexes.length-1]]=temp
-                this.save()
+        let indexes=[]
+            for(let i =0; i < this.flicks.length;i++){
+                const currentId=this.flicks[i].id.toString()
+                if(currentId===listI.dataset.id&&i>0){
+                
+                if(indexes.length>0){
+                        const temp=this.flicks[i]
+                        this.flicks[i]=this.flicks[indexes[indexes.length-1]]
+                        this.flicks[indexes[indexes.length-1]]=temp
+                        this.save()
+                    }
+                    break
                 }
-           
-            break
-        }
-        if(this.flicks[i].catagory===listI.dataset.catagory){
-                console.log(indexes)
-                indexes.push(i)}
-
-
-        }
+                if(this.flicks[i].catagory===listI.dataset.catagory){
+                        indexes.push(i)}
+            }
 
     }
 
@@ -176,30 +144,20 @@ let indexes=[]
         if(this.list.lastChild.previousSibling!==listI.nextSibling){
             this.list.insertBefore(nextSib,listI)
         }
-
         for(let i =this.flicks.length-1; i >= 0;i--){
             const currentId=this.flicks[i].id.toString()
             if(currentId===listI.dataset.id&&i<this.flicks.length-1){
-            
                  if(indexes.length>0){
-                console.log(this.flicks[i])
-                console.log(this.flicks[indexes[indexes.length-1]])
                 const temp=this.flicks[i]
                 this.flicks[i]=this.flicks[indexes[indexes.length-1]]
                 this.flicks[indexes[indexes.length-1]]=temp
                 this.save()
                 }
-                // const temp=this.flicks[i]
-                // this.flicks[i]=this.flicks[i+1]
-                // this.flicks[i+1]=temp
-                // this.save()
                 break
             }
             if(this.flicks[i].catagory===listI.dataset.catagory){
-                    console.log(indexes)
                     indexes.push(i)}
         }
-
     }
     promote(ev){
    
@@ -243,63 +201,31 @@ let indexes=[]
     }
 
 newCatagory(ev){
-//console.log(ev.currentTarget)
+
 const cat =ev.currentTarget
-
-
-
 this.currentCat=cat.dataset.catagory
-
 this.differentCat()
-// for(let j=0;j<nod.length-1;j++){
-//     //console.log(nod)
-//     console.log(nod[j])
-// nod[j].remove();j--
-
-// }
-// console.log(nod.length)
-// for(let i=this.flicks.length-1;i>=0 ;i--){
- 
-//    if(this.flicks[i].catagory===this.currentCat){
-       
-// const listItem =this.renderListItem(this.flicks[i])
-//    this.list.insertBefore(listItem,this.list.firstChild)
-// }
-
-// } 
-
 }
 differentCat(){
 const nod= this.list.children
-for(let j=0;j<nod.length-1;j++){
-    //console.log(nod)
-    console.log(nod[j])
+for(let j=0;j<nod.length-1;j++){  
 nod[j].remove();j--
-
 }
-console.log(nod.length)
+
 for(let i=this.flicks.length-1;i>=0 ;i--){
  
    if(this.flicks[i].catagory===this.currentCat){
        
-const listItem =this.renderListItem(this.flicks[i])
+    const listItem =this.renderListItem(this.flicks[i])
    this.list.insertBefore(listItem,this.list.firstChild)
 }
-
 } 
-
 }
-
-
-
-
 }
     const app = new App({
-  //formSelector: '#flick-form',
   listSelector: '#flick-list',
   catagoryList:'#catagory-list',
   templateSelector: '.flick.template',
   catagorySelector: '#catagory-form',
   catagoryTemp:'.catagory.template',
-
 })
