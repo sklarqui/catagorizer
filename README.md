ad7cef5d7d98aceaf46e824d3dfde9e2451ce07a
sklarqui <sklarqui@purdue.edu>
2017-06-12 22:17:19 -0400

    added a list, input, and catagory areas

---
+++ README.md
@@ -0,0 +1,13 @@
+#Day 5 Homework
+
+* Categorize stuff! Separate flicks into genres, or separate movies from TV shows! Make a whole new thing with categories, like messages separated into channels! Use your tremendous creativity! Exclamation point!
+
+* Or heck, just make separate lists based on the second input field you added in the weekend homework!
+
+##Super Mega Bonus Credit
+
+* Add a search field, and only show the results that match the search criteria. Here are some things you may wish to use while implementing search:
+
+* Array.prototype.find() (very similar to findIndex())
+* HTMLElement.dataset (remember those IDs we stored?)
+* display: none (Heck, you could add a class that includes this rule, and add and move that class from elements you wish to hide.)
---
+++ app.js
@@ -0,0 +1,305 @@
+class App{
+constructor(selectors){
+        this.flicks=[]
+        this.max=0
+  this.currentCat=''
+        this.list=document.querySelector(selectors.listSelector)
+        this.catList=document.querySelector(selectors.catagoryList)
+        this.template=document.querySelector(selectors.templateSelector)
+        this.catTemplate=document.querySelector(selectors.catagoryTemp)
+        document
+        .querySelector(selectors.catagorySelector)
+        .addEventListener('submit',this.addResponseViaForm.bind(this))
+        this.load()  
+}
+    addResponseViaForm(ev) {
+        ev.preventDefault()
+        const movieName= ev.target.response.value
+    
+        const f =ev.target
+        const flick = { 
+            id: this.max+1,
+            name: f.response.value,
+            catagory: f.catagory.value,
+            good: 'ehh',
+            }
+        this.addFlick(flick
+        )
+        console.log(flick.catagory)
+        this.currentCat=flick.catagory
+        this.differentCat()
+        f.reset()
+    }
+    addFlick(flick){
+
+const index =this.flicks.findIndex((currentFlick, i) =>{
+    return currentFlick.catagory===flick.catagory
+})
+
+if(flick.catagory===this.currentCat){
+ const listItem =this.renderListItem(flick)
+   this.list.insertBefore(listItem,this.list.firstChild)
+    }
+
+if(index===-1){
+   const cat =this.renderCatagories(flick)
+   this.catList.insertBefore(cat,this.catList.firstChild)
+}
+this.max++
+this.flicks.unshift(flick)
+this.save()
+
+}
+
+renderCatagories(flick){
+const item =this.catTemplate.cloneNode(true)
+ item.querySelector('.catagory-name').textContent=flick.catagory
+item.dataset.catagory= flick.catagory
+item.classList.remove('template')
+item.addEventListener('click',this.newCatagory.bind(this))
+return item
+}
+
+
+ renderListItem(flick){
+        const item =this.template.cloneNode(true)//document.createElement('.flick.template')
+       item.querySelector('.flick-name').textContent=flick.name
+       item.dataset.catagory=flick.catagory
+      //  item.querySelector('.flick-year').textContent=flick.cata
+        item.dataset.id = flick.id
+        item.classList.remove('template')
+        item.querySelector('.remove').addEventListener('click',this.removeMovie.bind(this))
+        item.querySelector('.favorite').addEventListener('click',this.promote.bind(this))
+        item.querySelector('.up').addEventListener('click',this.flipUp.bind(this))
+        item.querySelector('.down').addEventListener('click',this.flipDown.bind(this))
+        item.querySelector('.flick-name').addEventListener('input',this.edited.bind(this))
+   if(flick.good==='red'){
+       this.prom(item)
+   }
+    return item   
+}
+
+removeMovie(ev) {
+const listI=ev.target.closest('.flick')
+console.log(listI)
+listI.remove()
+
+
+//remove from arrray in general splice(?,1)
+
+for(let i =0; i < this.flicks.length;i++){
+    console.log(listI.dataset.id)
+    
+    const currentId=this.flicks[i].id.toString()
+    console.log(currentId)
+if(currentId===listI.dataset.id){
+    this.flicks.splice(i,1)
+    this.save()
+    break
+}
+
+}
+//JSON
+}
+save(){
+    console.log(this.flicks)
+localStorage.setItem('flicks',JSON.stringify(this.flicks))
+}
+
+load(){
+//get the json string out of local storage, 
+const flicksJSON=localStorage.getItem('flicks')
+//turn it into an array
+const flicksArray=JSON.parse(flicksJSON)
+//this.flicks=flicksArray
+console.log(flicksArray)
+if(flicksArray){
+flicksArray.reverse().map(this.addFlick.bind(this))
+
+}
+for(let i=0;i<this.flicks.length;i++){
+if(this.flicks[i].id>this.max){
+this.max=this.flicks[i].id+1
+}
+}
+}
+
+
+ flipUp(ev){
+
+        const listI=ev.target.closest('.flick')
+
+
+
+        if(this.list.firstChild!==listI){
+            const prevSib=listI.previousSibling
+            this.list.insertBefore(listI,prevSib)
+
+        }
+        console.log(listI)
+let indexes=[]
+        for(let i =0; i < this.flicks.length;i++){
+            const currentId=this.flicks[i].id.toString()
+           
+
+        if(currentId===listI.dataset.id&&i>0){
+          
+            if(indexes.length>0){
+                console.log(this.flicks[i])
+                console.log(this.flicks[indexes[indexes.length-1]])
+                const temp=this.flicks[i]
+                this.flicks[i]=this.flicks[indexes[indexes.length-1]]
+                this.flicks[indexes[indexes.length-1]]=temp
+                this.save()
+                }
+           
+            break
+        }
+        if(this.flicks[i].catagory===listI.dataset.catagory){
+                console.log(indexes)
+                indexes.push(i)}
+
+
+        }
+
+    }
+
+    flipDown(ev){
+
+let indexes=[]
+
+        const listI=ev.target.closest('.flick')
+        const nextSib=listI.nextSibling
+        
+
+        
+        if(this.list.lastChild.previousSibling!==listI.nextSibling){
+            this.list.insertBefore(nextSib,listI)
+        }
+
+        for(let i =this.flicks.length-1; i >= 0;i--){
+            const currentId=this.flicks[i].id.toString()
+            if(currentId===listI.dataset.id&&i<this.flicks.length-1){
+            
+                 if(indexes.length>0){
+                console.log(this.flicks[i])
+                console.log(this.flicks[indexes[indexes.length-1]])
+                const temp=this.flicks[i]
+                this.flicks[i]=this.flicks[indexes[indexes.length-1]]
+                this.flicks[indexes[indexes.length-1]]=temp
+                this.save()
+                }
+                // const temp=this.flicks[i]
+                // this.flicks[i]=this.flicks[i+1]
+                // this.flicks[i+1]=temp
+                // this.save()
+                break
+            }
+            if(this.flicks[i].catagory===listI.dataset.catagory){
+                    console.log(indexes)
+                    indexes.push(i)}
+        }
+
+    }
+    promote(ev){
+   
+        const listI=ev.target.closest('.flick')
+        if(listI.style.backgroundColor!=='red'){
+         listI.style.backgroundColor='red'
+        }
+        else{
+         listI.style.backgroundColor='white'
+        }
+
+        for(let i =0; i < this.flicks.length;i++){
+            const currentId=this.flicks[i].id.toString()
+            
+        if(currentId===listI.dataset.id){
+            
+            this.flicks[i].good=listI.style.backgroundColor
+        
+            this.save()
+            break
+        }
+
+        }
+
+    }
+    prom(listI){
+         listI.style.backgroundColor='red'
+    }
+    edited(ev){
+
+        const listI=ev.target.closest('.flick')
+
+        for(let i =0; i < this.flicks.length;i++){
+            const currentId=this.flicks[i].id.toString()            
+            if(currentId===listI.dataset.id){
+                this.flicks[i].name=ev.target.textContent
+                this.save()
+                break
+            }
+        }
+    }
+
+newCatagory(ev){
+//console.log(ev.currentTarget)
+const cat =ev.currentTarget
+
+
+
+this.currentCat=cat.dataset.catagory
+
+this.differentCat()
+// for(let j=0;j<nod.length-1;j++){
+//     //console.log(nod)
+//     console.log(nod[j])
+// nod[j].remove();j--
+
+// }
+// console.log(nod.length)
+// for(let i=this.flicks.length-1;i>=0 ;i--){
+ 
+//    if(this.flicks[i].catagory===this.currentCat){
+       
+// const listItem =this.renderListItem(this.flicks[i])
+//    this.list.insertBefore(listItem,this.list.firstChild)
+// }
+
+// } 
+
+}
+differentCat(){
+const nod= this.list.children
+for(let j=0;j<nod.length-1;j++){
+    //console.log(nod)
+    console.log(nod[j])
+nod[j].remove();j--
+
+}
+console.log(nod.length)
+for(let i=this.flicks.length-1;i>=0 ;i--){
+ 
+   if(this.flicks[i].catagory===this.currentCat){
+       
+const listItem =this.renderListItem(this.flicks[i])
+   this.list.insertBefore(listItem,this.list.firstChild)
+}
+
+} 
+
+}
+
+
+
+
+}
+    const app = new App({
+  //formSelector: '#flick-form',
+  listSelector: '#flick-list',
+  catagoryList:'#catagory-list',
+  templateSelector: '.flick.template',
+  catagorySelector: '#catagory-form',
+  catagoryTemp:'.catagory.template',
+
+})
